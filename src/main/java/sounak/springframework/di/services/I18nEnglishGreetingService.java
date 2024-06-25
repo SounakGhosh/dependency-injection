476d4f638d7cd14a8cdacdf8c4566c585dbd9f58
sounak <sounak.peter@gmail.com>
2024-06-25 20:43:18 +0530

    Default Profile

--- src/main/java/sounak/springframework/di/services/I18nEnglishGreetingService.java
+++ src/main/java/sounak/springframework/di/services/I18nEnglishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by sounak on 25-06-2024.
  */
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
