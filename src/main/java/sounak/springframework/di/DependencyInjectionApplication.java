b2d8557f5f309d902e0a0b993d70ad69b7b17e65
sounak <sounak.peter@gmail.com>
2024-06-25 21:39:38 +0530

    Deleting unnecessary blank lines

--- src/main/java/sounak/springframework/di/DependencyInjectionApplication.java
+++ src/main/java/sounak/springframework/di/DependencyInjectionApplication.java
@@ -19,38 +19,29 @@ public class DependencyInjectionApplication {
         System.out.println();
 
         I18nController i18nController = (I18nController) context.getBean("i18nController");
-
         System.out.println(i18nController.sayHello());
         System.out.println();
 
         System.out.println("--- Primary Bean ---");
-
         MyController myController = (MyController) context.getBean("myController");
-
         System.out.println(myController.sayHello());
         System.out.println();
 
         System.out.println("--- Property Injection ---");
-
         PropertyInjectedController propertyInjectedController =
                 (PropertyInjectedController) context.getBean("propertyInjectedController");
-
         System.out.println(propertyInjectedController.getGreeting());
         System.out.println();
 
         System.out.println("--- Setter Injection ---");
-
         SetterInjectedController setterInjectedController =
                 (SetterInjectedController) context.getBean("setterInjectedController");
-
         System.out.println(setterInjectedController.getGreeting());
         System.out.println();
 
         System.out.println("--- Constructor Injection ---");
-
         ConstructorInjectedController constructorInjectedController =
                 (ConstructorInjectedController) context.getBean("constructorInjectedController");
-
         System.out.println(constructorInjectedController.getGreeting());
 
     }
