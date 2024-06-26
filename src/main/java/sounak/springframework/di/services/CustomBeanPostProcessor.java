b52b07be12efddb46f7719dcb01c1c3fde56da0e
sounak <sounak.peter@gmail.com>
2024-06-26 09:13:39 +0530

    Spring Bean Life Cycle

---
+++ src/main/java/sounak/springframework/di/services/CustomBeanPostProcessor.java
@@ -0,0 +1,30 @@
+package sounak.springframework.di.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by sounak on 26-06-2024.
+ */
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
